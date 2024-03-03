9bb457fc7928db28aad6cc4d46ad8765573042b3
goliagri <griffingoli^C@gmail.com>
2024-03-03 09:22:44 -0800

    probs 1 and 2

---
+++ Problems/p001.py
@@ -0,0 +1,24 @@
+import math 
+
+def p0001(x1, x2, max_val=1000): #max is not inclusive
+    '''
+    Get sum of integers (0,max_val) which are divisible by either x1 or x2
+    '''
+    x_lcm = math.lcm(x1, x2)
+
+    def get_seq_sum(factor, max_):
+        seq_len = (max_-1) // factor
+        return factor * ((seq_len+1) * seq_len)//2 
+    
+    return get_seq_sum(x1, max_val) + get_seq_sum(x2, max_val) - get_seq_sum(x_lcm, max_val)
+
+def tests():
+    print(p0001(3,5,1000))
+
+def main():
+    tests()
+
+
+if __name__ == '__main__':
+    main()
+
---
+++ Problems/p002.py
@@ -0,0 +1,25 @@
+import math 
+import numpy as np
+
+def p0002(max_val): #max is inclusive
+    X = np.ones((2,1))
+    X[0][0] += 1
+    transform = np.array([[1,1],[1,0]])
+    res = 0
+    while X[0][0] <= max_val:
+        if X[0][0] % 2 == 0:
+            res += X[0][0]
+        X = np.matmul(transform, X)
+    return int(res)
+
+
+def tests():
+    print(p0002(4e6))
+
+def main():
+    tests()
+
+
+if __name__ == '__main__':
+    main()
+
